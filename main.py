0e60195d2dd5697fb8622ae0075c773a5b1875f7
Andrew Barisser <barisser@protonmail.com>
2019-07-08 20:40:19 -0700

    simulator code

---
+++ main.py
@@ -0,0 +1,26 @@
+import gym
+import pandas as pd
+
+
+def simulate(environment_name, model, env=None, max_iterations=10**3, render=False):
+    if env is None:
+        env = gym.make(environment_name)
+    observation = env.reset()
+
+    history = [[0, observation, None, 0]] # i, obs, action, interval_reward
+
+    for i in range(max_iterations):
+        if render:
+            env.render()
+
+        action = model.choose_action(observation)
+        observation, reward, done, info = env.step(action)
+        history.append([i + 1, observation, action, reward])
+
+        if done:
+            break
+
+    return pd.DataFrame(history)
+
+
+
